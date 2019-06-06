6b9b41bd9ddf5592e4e6b8594b9f7129fdd8a306
Juan J Salazar <juan_salazar96@hotmail.com>
2019-06-05 17:51:28 -0700

    Test with two columns

---
+++ h2/src/test/org/h2/test/db/MVUnitTest.java
@@ -0,0 +1,80 @@
+package org.h2.test.db;
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.h2.tools.DeleteDbFiles;
+import org.junit.Test;
+
+
+
+public class MVUnitTest {
+	
+	  @Test
+	  public void MVTest() throws ClassNotFoundException, SQLException {
+		  
+	        // delete the database named 'test' in the user home directory
+	        DeleteDbFiles.execute("~", "test", true);
+
+	        Class.forName("org.h2.Driver");
+	        Connection conn = DriverManager.getConnection("jdbc:h2:~/test");
+	        Statement stat = conn.createStatement();		  
+	        ResultSet rs;
+	        
+	        
+	        stat.execute("CREATE TABLE test(id int primary key, name varchar(255))");
+	        stat.execute("INSERT INTO test VALUES(1, 'Hello')");
+	        stat.execute("INSERT INTO test VALUES(2, 'World')");
+	        stat.execute("INSERT INTO test VALUES(3, '!!!')");
+
+	        stat.execute("CREATE MATERIALIZED VIEW mvname AS SELECT id, name FROM test");
+	        
+	        //Original values in the underlying table
+	        rs = stat.executeQuery("select * from mvname");
+	        rs.next();
+	        assertEquals(rs.getString("id"), "1");	        
+	        assertEquals(rs.getString("name"), "Hello");
+	        rs.next();
+	        assertEquals(rs.getString("id"), "2");	        
+	        assertEquals(rs.getString("name"), "World");
+	        rs.next();
+	        assertEquals(rs.getString("id"), "3");	        
+	        assertEquals(rs.getString("name"), "!!!");
+	        
+	        
+	        //Insert Values into underlying table after creating the materialized view
+	        stat.execute("INSERT INTO test VALUES(4, 'CSC')");
+	        stat.execute("INSERT INTO test VALUES(5, '468')");
+	        stat.execute("INSERT INTO test VALUES(6, 'DBMS')");
+	        rs = stat.executeQuery("select * from mvname");
+
+	        //Check Results
+	        rs.next();
+	        assertEquals(rs.getString("id"), "1");	        
+	        assertEquals(rs.getString("name"), "Hello");
+	        rs.next();
+	        assertEquals(rs.getString("id"), "2");	        
+	        assertEquals(rs.getString("name"), "World");
+	        rs.next();
+	        assertEquals(rs.getString("id"), "3");	        
+	        assertEquals(rs.getString("name"), "!!!");
+	        rs.next();
+	        assertEquals(rs.getString("id"), "4");	        
+	        assertEquals(rs.getString("name"), "CSC");
+	        rs.next();
+	        assertEquals(rs.getString("id"), "5");	        
+	        assertEquals(rs.getString("name"), "468");
+	        rs.next();
+	        assertEquals(rs.getString("id"), "6");	        
+	        assertEquals(rs.getString("name"), "DBMS");
+	        
+	        stat.close();
+	        conn.close();
+	        
+	  }
+	
+}
